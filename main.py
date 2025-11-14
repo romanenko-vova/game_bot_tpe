ed39982608dc2046bbe78a247bc3f03b0e6fcae4
Romanenko Vladimir <bonyshd@gmail.com>
2025-11-14 07:53:23 +0300

    1 Урок

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
-print("hello world")
+import logging
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+)
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+
+# callback
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # update - полная информация о том, что произошло
+    # update.effective_chat - вся инфа о чате
+    # update.effective_user - вся инфа о пользователе
+    # update.effective_message - вся инфа о сообщении
+    # update.effective_message.text - текст сообщения
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"Привет {update.effective_user.first_name}!",
+    )
+    
+async def biba(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="боба",
+    )
+
+
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(os.getenv("TOKEN")).build()
+
+    # handler - обработчик
+    # CommandHandler - обработчик команд
+    # MessageHandler - обработчик сообщений
+    
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("biba", biba))
+
+    application.run_polling()
