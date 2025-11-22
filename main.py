e38f6936d24e110f579a5286cd66d98555307c47
Romanenko Vladimir <bonyshd@gmail.com>
2025-11-22 17:42:46 +0300

    Урок 2

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
     CommandHandler,
+    MessageHandler,
+    filters,
+    ConversationHandler,
 )
 from dotenv import load_dotenv
 
@@ -16,6 +19,8 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+MAINMENU, TALK = range(2)
+
 
 # callback
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -26,14 +31,39 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # update.effective_message.text - текст сообщения
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=f"Привет {update.effective_user.first_name}!",
+        text=f"Привет {update.effective_user.first_name}!\n\nНапиши команду:\n /talk чтобы поговорить со мной.\n /biba чтобы получить бобу.",
     )
-    
+    return MAINMENU
+
+
 async def biba(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="боба",
     )
+
+
+async def talk_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Чтобы поговорить со мной, просто напиши любой текст.",
+    )
+    return TALK
+
+
+async def talk(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.effective_message.text
+    if "привет" in text.lower():
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Здарово, брат!",
+        )
+    # чтобы тут обрабатывались 5 рандомных фраз
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text,
+        )
 
 
 if __name__ == "__main__":
@@ -42,8 +72,23 @@ if __name__ == "__main__":
     # handler - обработчик
     # CommandHandler - обработчик команд
     # MessageHandler - обработчик сообщений
-    
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("biba", biba))
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            MAINMENU: [
+                CommandHandler("talk", talk_start),
+                CommandHandler("biba", biba),
+            ],
+            TALK: [MessageHandler(filters.TEXT & ~filters.COMMAND, talk)],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    application.add_handler(conv_handler)
+
+    # & - и
+    # | - или
+    # ~ - не
 
     application.run_polling()
