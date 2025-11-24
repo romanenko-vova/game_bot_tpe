c3b9af8587068e3043437465f5f53b856f74f19f
Romanenko Vladimir <bonyshd@gmail.com>
2025-11-24 11:39:17 +0300

    Урок 3

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import logging
-from telegram import Update
+from telegram import (
+    Update,
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 from telegram.ext import (
     ApplicationBuilder,
     ContextTypes,
@@ -7,6 +13,7 @@ from telegram.ext import (
     MessageHandler,
     filters,
     ConversationHandler,
+    CallbackQueryHandler
 )
 from dotenv import load_dotenv
 
@@ -19,7 +26,7 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-MAINMENU, TALK = range(2)
+MAINMENU, TALK, BIBA = range(3)
 
 
 # callback
@@ -29,17 +36,54 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # update.effective_user - вся инфа о пользователе
     # update.effective_message - вся инфа о сообщении
     # update.effective_message.text - текст сообщения
+    keyboard = [
+        [InlineKeyboardButton("Режим разговора", callback_data="talk")],
+        [InlineKeyboardButton("Режим бибы", callback_data="biba")],
+    ]
+    markup = InlineKeyboardMarkup(keyboard)
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=f"Привет {update.effective_user.first_name}!\n\nНапиши команду:\n /talk чтобы поговорить со мной.\n /biba чтобы получить бобу.",
+        reply_markup=markup
     )
     return MAINMENU
 
 
 async def biba(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # query - запрос
+    query = update.callback_query
+    await query.answer()
+    
+    keyboard = [["биба"], ["боба"]]
+    markup = ReplyKeyboardMarkup(keyboard)
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="боба",
+        chat_id=update.effective_chat.id, text="боба", reply_markup=markup
+    )
+    return BIBA
+
+
+async def say_boba(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        ["биба"],
+        ["боба"],
+        [KeyboardButton("поделиться контактом", request_contact=True)],
+    ]
+    markup = ReplyKeyboardMarkup(keyboard)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="боба", reply_markup=markup
+    )
+
+
+async def say_biba(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="биба"
+    )
+
+
+async def say_contact(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    phone_number = update.effective_message.contact.phone_number
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=f"Ваш номер: {phone_number}"
     )
 
 
@@ -79,8 +123,15 @@ if __name__ == "__main__":
             MAINMENU: [
                 CommandHandler("talk", talk_start),
                 CommandHandler("biba", biba),
+                CallbackQueryHandler(biba, pattern="biba"),
+                CallbackQueryHandler(talk_start, pattern="talk"),
             ],
             TALK: [MessageHandler(filters.TEXT & ~filters.COMMAND, talk)],
+            BIBA: [
+                MessageHandler(filters.Regex("^биба$"), say_boba),
+                MessageHandler(filters.Regex("^боба$"), say_biba),
+                MessageHandler(filters.CONTACT, say_contact),
+            ],
         },
         fallbacks=[CommandHandler("start", start)],
     )
